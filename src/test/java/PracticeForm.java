d12b066d09203746321c87eabe122ad2fc631cc0
AlexandrEl18 <danilin4466@mail.ru>
2025-08-07 17:32:07 +0200

    PracticeForm

--- src/test/java/PracticeForm.java
+++ src/test/java/PracticeForm.java
@@ -2,22 +2,18 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PracticeForm {
-
     @BeforeEach
     void setUp() {
         System.setProperty("selenide.holdBrowserOpen", "true");
         Configuration.browserSize = "1920x1080";
     }
-
     @Test
     void fillFormTest() {
         open("https://demoqa.com/automation-practice-form");
@@ -26,7 +22,6 @@ public class PracticeForm {
         $("[id=userEmail]").setValue("Bobrow@mail.ru");
         $(byText("Male")).click();
         $("[id=userNumber]").setValue("9995556677");
-
         // Клик по полю (открыть календарь)
         $("[id=dateOfBirthInput]").click();
         // Выбрать месяц
@@ -37,19 +32,16 @@ public class PracticeForm {
         $(".react-datepicker__day--010").click();
         $("[id=subjectsInput]").setValue("Math").pressEnter();
         $(byText("Sports")).click();
-
         //загрузка фото
         $("[id=uploadPicture]").uploadFile(new File("/Users/aleksandrael/Downloads/IMG_1366.HEIC"));
         $("[id=currentAddress]").setValue("123 Maple St, Springfield, IL 62704");
 // Скролл к этому лементу и клик
         $("[id=state]").scrollTo().click();
         $(byText("NCR")).click();
-
         $("[id=city]").click();
         $(byText("Delhi")).click();
-
         $("[id=submit]").click();
-
+        //проверка совпадений в итоговой таблице
         $(".table").shouldHave(
                 text("Student Name"),
                 text("Алексей Бобров"),
