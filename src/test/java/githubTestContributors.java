d882514b6b264f0251f40680df8e512c9703c061
AlexandrEl18 <danilin4466@mail.ru>
2025-08-08 20:17:43 +0200

    PracticeForm

--- src/test/java/githubTestContributors.java
+++ src/test/java/githubTestContributors.java
@@ -1,13 +1,10 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static java.awt.SystemColor.text;
 import static java.lang.Thread.sleep;
 
 public class githubTestContributors {
@@ -15,7 +12,6 @@ public class githubTestContributors {
         void setUp() {
             Configuration.browserSize = "1920x1080";
         }
-
         @Test
         void Test1() throws InterruptedException {
             open("https://github.com/selenide/selenide");
