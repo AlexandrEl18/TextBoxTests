5d41245b86456537fff7b7a76020962763536f1f
Alexandr El <ael18@vk.com>
2025-08-31 10:18:00 +0200

    property

--- src/test/java/pages/components/TestBase.java
+++ src/test/java/pages/components/TestBase.java
@@ -6,17 +6,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import io.qameta.allure.selenide.AllureSelenide;
+import static com.codeborne.selenide.Configuration.*;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll(){
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-    }
+        browser = System.getProperty("browser", "chrome");
+        browserVersion = System.getProperty("browserVersion", "128.0");
+        browserSize = System.getProperty("browserSize", "1920x1080");
+       // Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+    }    //  ./gradlew clean registration_test -Dbrowser=chrome -DbrowserVersion=127.0 -DbrowserSize=320x240
 
     @BeforeEach
     void addListener() {
